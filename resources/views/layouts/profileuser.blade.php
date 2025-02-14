d7d51b01b6b9791569a2517f2b35cd609a72b5d7
zapranpran <zahranraifal@gmail.com>
2025-02-14 15:03:45 +0700

    hayuu

--- resources/views/layouts/profileuser.blade.php
+++ resources/views/layouts/profileuser.blade.php
@@ -1,37 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Dashboard User</title>
-    <meta
-      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-      name="viewport"
-       />
-    <link
-      rel="icon"
-      href="{{asset('assets/img/smk assalaam.png') }}"
-      type="image/x-icon"
-    />
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
+    <link rel="icon" href="{{ asset('assets/img/smk assalaam.png') }}" type="image/x-icon" />
 
     <!-- Fonts and icons -->
     <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
     <script>
-      WebFont.load({
-        google: { families: ["Public Sans:300,400,500,600,700"] },
-        custom: {
-          families: [
-            "Font Awesome 5 Solid",
-            "Font Awesome 5 Regular",
-            "Font Awesome 5 Brands",
-            "simple-line-icons",
-          ],
-          urls: ["assets/css/fonts.min.css"],
-        },
-        active: function () {
-          sessionStorage.fonts = true;
-        },
-      });
+        WebFont.load({
+            google: {
+                families: ["Public Sans:300,400,500,600,700"]
+            },
+            custom: {
+                families: [
+                    "Font Awesome 5 Solid",
+                    "Font Awesome 5 Regular",
+                    "Font Awesome 5 Brands",
+                    "simple-line-icons",
+                ],
+                urls: [
+                    "{{ asset('assets/css/fonts.min.css') }}"
+                ],
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            },
+        });
     </script>
 
     <!-- CSS Files -->
@@ -41,46 +39,44 @@
 
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
-  </head>
-  <body>
+    @yield('css')
+</head>
+
+<body>
     <div class="wrapper">
-      <!-- Sidebar -->
+        <!-- Sidebar -->
         @include('include.profile.sidebar')
-      <!-- End Sidebar -->
+        <!-- End Sidebar -->
 
-      <div class="main-panel">
-        <div class="main-header">
-          <div class="main-header-logo">
-            <!-- Logo Header -->
-            <div class="logo-header" data-background-color="dark">
-              <a href="index.html" class="logo">
-                <img
-                  src="assets/img/kaiadmin/logo_light.svg"
-                  alt="navbar brand"
-                  class="navbar-brand"
-                  height="20"
-                />
-              </a>
-              <div class="nav-toggle">
-                <button class="btn btn-toggle toggle-sidebar">
-                  <i class="gg-menu-right"></i>
-                </button>
-                <button class="btn btn-toggle sidenav-toggler">
-                  <i class="gg-menu-left"></i>
-                </button>
-              </div>
-              <button class="topbar-toggler more">
-                <i class="gg-more-vertical-alt"></i>
-              </button>
+        <div class="main-panel">
+            <div class="main-header">
+                <div class="main-header-logo">
+                    <!-- Logo Header -->
+                    <div class="logo-header" data-background-color="dark">
+                        <a href="index.html" class="logo">
+                            <img src="{{ asset('assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand"
+                                class="navbar-brand" height="20" />
+                        </a>
+                        <div class="nav-toggle">
+                            <button class="btn btn-toggle toggle-sidebar">
+                                <i class="gg-menu-right"></i>
+                            </button>
+                            <button class="btn btn-toggle sidenav-toggler">
+                                <i class="gg-menu-left"></i>
+                            </button>
+                        </div>
+                        <button class="topbar-toggler more">
+                            <i class="gg-more-vertical-alt"></i>
+                        </button>
+                    </div>
+                    <!-- End Logo Header -->
+                </div>
+                <!-- Navbar Header -->
+                @include('include.profile.navbar')
+                <!-- End Navbar -->
             </div>
-            <!-- End Logo Header -->
-          </div>
-          <!-- Navbar Header -->
-          @include('include.profile.navbar')
-          <!-- End Navbar -->
-        </div>
 
-        {{-- main content --}}
+            {{-- main content --}}
             <main class="main-wrapper">
                 <div class="main-content">
                     <div class="row">
@@ -88,13 +84,13 @@
                     </div>
                 </div>
             </main>
-        {{-- akhir main content --}}
+            {{-- akhir main content --}}
             {{-- footer --}}
 
-      {{-- footer --}}
+            {{-- footer --}}
 
-      <!-- Custom template | don't include it in your project! -->
-      {{-- <div class="custom-template">
+            <!-- Custom template | don't include it in your project! -->
+            {{-- <div class="custom-template">
         <div class="title">Settings</div>
         <div class="custom-content">
           <div class="switcher">
@@ -286,43 +282,44 @@
           <i class="icon-settings"></i>
         </div>
       </div> --}}
-      <!-- End Custom template -->
-    </div>
-    <!--   Core JS Files   -->
-    <script src="{{ asset('assets/js/core/jquery-3.7.1.min.js') }}"></script>
-    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+            <!-- End Custom template -->
+        </div>
+        <!--   Core JS Files   -->
+        <script src="{{ asset('assets/js/core/jquery-3.7.1.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
 
-    <!-- jQuery Scrollbar -->
-    <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+        <!-- jQuery Scrollbar -->
+        <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
 
-    <!-- Chart JS -->
-    <script src="{{ asset('assets/js/plugin/chart.js/chart.min.js') }}"></script>
+        <!-- Chart JS -->
+        <script src="{{ asset('assets/js/plugin/chart.js/chart.min.js') }}"></script>
 
-    <!-- jQuery Sparkline -->
-    <script src="{{ asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
+        <!-- jQuery Sparkline -->
+        <script src="{{ asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
 
-    <!-- Chart Circle -->
-    <script src="{{ asset('assets/js/plugin/chart-circle/circles.min.js') }}"></script>
+        <!-- Chart Circle -->
+        <script src="{{ asset('assets/js/plugin/chart-circle/circles.min.js') }}"></script>
 
-    <!-- Datatables -->
-    <script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
+        <!-- Datatables -->
+        <script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
 
-    <!-- Bootstrap Notify -->
-    <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+        <!-- Bootstrap Notify -->
+        <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
 
-    <!-- jQuery Vector Maps -->
-    <script src="{{ asset('assets/js/plugin/jsvectormap/jsvectormap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/plugin/jsvectormap/world.js') }}"></script>
+        <!-- jQuery Vector Maps -->
+        <script src="{{ asset('assets/js/plugin/jsvectormap/jsvectormap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugin/jsvectormap/world.js') }}"></script>
 
-    <!-- Sweet Alert -->
-    <script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
+        <!-- Sweet Alert -->
+        <script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
 
-    <!-- Kaiadmin JS -->
-    <script src="{{ asset('assets/js/kaiadmin.min.js') }}"></script>
+        <!-- Kaiadmin JS -->
+        <script src="{{ asset('assets/js/kaiadmin.min.js') }}"></script>
+        @@stack('js')
 
-    <!-- Kaiadmin DEMO methods, don't include it in your project! -->
-    {{-- <script src="{{ asset('assets/js/setting-demo.js') }}"></script>
+        <!-- Kaiadmin DEMO methods, don't include it in your project! -->
+        {{-- <script src="{{ asset('assets/js/setting-demo.js') }}"></script>
     <script src="{{ asset('assets/js/demo.js') }}"></script>
     <script>
       $("#lineChart").sparkline([102, 109, 120, 99, 110, 105, 115], {
@@ -352,5 +349,6 @@
         fillColor: "rgba(255, 165, 52, .14)",
       });
     </script> --}}
-  </body>
+</body>
+
 </html>
--- resources/views/user/peminjaman/create.blade.php
+++ resources/views/user/peminjaman/create.blade.php
@@ -1,98 +1,123 @@
 <title>Peminjaman</title>
 @extends('layouts.profileuser')
+<!-- Select2 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet">
+
+
 @section('content')
-<h3 class="mb-0 text-uppercase pb-3">PINJAMAN BUKU</h3>
-<hr>
-<div class="card">
-    <div class="card-body">
-        @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-        @endif
-        {{-- <a href="{{ route('peminjaman.create') }}" class="btn btn-grd btn-primary px-5 mb-2">Tambah Data Peminjaman</a> --}}
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header"> {{__('DATA PEMINJAMAN')}}  </div>
-                        <div class="card-body">
-                            <form class="row g-3" method="POST" action="{{ route('peminjaman.store') }}" enctype="multipart/form-data">
-                                @csrf
-                                @if ($errors->has('jumlah_pinjam'))
-                                <div class="alert alert-danger">
-                                    {{ $errors->first('jumlah_pinjam') }}
-                                </div>
-                                @endif
+    <h3 class="mb-0 text-uppercase pb-3">PINJAMAN BUKU</h3>
+    <hr>
+    <div class="card">
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+            {{-- <a href="{{ route('peminjaman.create') }}" class="btn btn-grd btn-primary px-5 mb-2">Tambah Data Peminjaman</a> --}}
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-header"> {{ __('DATA PEMINJAMAN') }} </div>
+                            <div class="card-body">
+                                <form class="row g-3" method="POST" action="{{ route('peminjaman.store') }}"
+                                    enctype="multipart/form-data">
+                                    @csrf
+                                    @if ($errors->has('jumlah_pinjam'))
+                                        <div class="alert alert-danger">
+                                            {{ $errors->first('jumlah_pinjam') }}
+                                        </div>
+                                    @endif
 
-                                @if (session('success'))
-                                <div class="alert alert-success">
-                                    {{ session('success') }}
-                                </div>
-                                @endif
+                                    @if (session('success'))
+                                        <div class="alert alert-success">
+                                            {{ session('success') }}
+                                        </div>
+                                    @endif
 
-                                @if ($errors->has('id_buku'))
-                                <div class="alert alert-danger">
-                                    {{ $errors->first('id_buku') }}
-                                </div>
-                                @endif
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Nama Peminjam</label>
-                                    <div class="position-relative">
-                                        <input class="form-control mb-3" type="text" name="nama_peminjam" placeholder="Nama" value="{{ Auth::user()->name }}" required readonly>
+                                    @if ($errors->has('id_buku'))
+                                        <div class="alert alert-danger">
+                                            {{ $errors->first('id_buku') }}
+                                        </div>
+                                    @endif
+                                    <div class="col-md-6">
+                                        <label for="input13" class="form-label">Nama Peminjam</label>
+                                        <div class="position-relative">
+                                            <input class="form-control mb-3" type="text" name="nama_peminjam"
+                                                placeholder="Nama" value="{{ Auth::user()->name }}" required readonly>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Tanggal Peminjaman</label>
-                                    <input class="form-control mb-3" type="date" name="tanggal_pinjam" value="{{ $sekarang }}" required readonly>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Batas Pengembalian</label>
-                                    <input class="form-control mb-3" type="date" name="batas_pinjam" value="{{ $batastanggal }}" required readonly>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <label for="input19" class="form-label">Status</label>
-                                    <select id="input19" name="status" class="form-select" required>
-                                        <option selected="">Pilih...</option>
-                                        <option value="0">Pinjam</option>
-                                        <option value="1">Kembalikan</option>
-                                    </select>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Nama Buku</label>
-                                    <select class="form-control mb-3" name="id_buku" placeholder="Buku" required>
-                                        @foreach ($buku as $data)
-                                        <option value="{{ $data->id }}">{{ $data->judul }} </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Jumlah</label>
-                                    <input class="form-control mb-3" type="number" name="jumlah" placeholder="Jumlah" required>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <label for="input13" class="form-label">Tanggal Pengembalian</label>
-                                    <input class="form-control mb-3" type="date" name="tanggal_kembali" required>
-                                </div>
-
-                                <div class="col-md-12">
-                                    <div class="d-md-flex d-grid align-items-center gap-3">
-                                        <a href="{{ url('user')}}" class="btn btn-danger px-4">Batal</a>
-                                        <button type="submit" class="btn btn-success px-4">Simpan</button>
+                                    <div class="col-md-6">
+                                        <label for="input13" class="form-label">Tanggal Peminjaman</label>
+                                        <input class="form-control mb-3" type="date" name="tanggal_pinjam"
+                                            value="{{ $sekarang }}" required readonly>
                                     </div>
-                                </div>
-                            </form>
+
+                                    <div class="col-md-6">
+                                        <label for="input13" class="form-label">Batas Pengembalian</label>
+                                        <input class="form-control mb-3" type="date" name="batas_pinjam"
+                                            value="{{ $batastanggal }}" required readonly>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label for="input19" class="form-label">Status</label>
+                                        <select id="input19" name="status" class="form-select" required>
+                                            <option selected="">Pilih...</option>
+                                            <option value="0">Pinjam</option>
+                                            <option value="1">Kembalikan</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label class="form-label">Nama Buku</label>
+                                        <select id="id_buku" class="form-control" name="id_buku" required>
+                                            <option selected disabled>- Pilih Buku -</option>
+                                            @foreach ($buku as $data)
+                                                <option value="{{ $data->id }}">{{ $data->judul }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label for="input13" class="form-label">Jumlah</label>
+                                        <input class="form-control mb-3" type="number" name="jumlah" placeholder="Jumlah"
+                                            required>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <label for="input13" class="form-label">Tanggal Pengembalian</label>
+                                        <input class="form-control mb-3" type="date" name="tanggal_kembali" required>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="d-md-flex d-grid align-items-center gap-3">
+                                            <a href="{{ url('user') }}" class="btn btn-danger px-4">Batal</a>
+                                            <button type="submit" class="btn btn-success px-4">Simpan</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- jQuery (Jika belum ada) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                $('#id_buku').select2({
+                    placeholder: "Cari Buku...",
+                    allowClear: true
+                });
+            });
+        </script>
+    @endpush
 @endsection
